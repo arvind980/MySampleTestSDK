7211174641db5950b7a3c9f3e9b23c9ab1357f65
arvind <arvindkumar@Mac-mini.local>
2022-10-17 09:56:53 +0530

    my first commit

---
+++ MySampleTestSDK/MySampleTestSDK/MySampleTestSDK.h
@@ -0,0 +1,18 @@
+//
+//  MySampleTestSDK.h
+//  MySampleTestSDK
+//
+//  Created by Arvind Kumar on 17/10/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MySampleTestSDK.
+FOUNDATION_EXPORT double MySampleTestSDKVersionNumber;
+
+//! Project version string for MySampleTestSDK.
+FOUNDATION_EXPORT const unsigned char MySampleTestSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MySampleTestSDK/PublicHeader.h>
+
+
